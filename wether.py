1a2f3e00ce302497f41bff282a59e58f544c45cf
Arun7426 <137769764+Arun7426@users.noreply.github.com>
2024-01-15 19:32:22 +0530

    Add files via upload

---
+++ wether.py
@@ -0,0 +1,35 @@
+import requests
+
+def get_weather(api_key, city):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q': city,
+        'appid': api_key,
+        'units': 'metric'  # Use 'imperial' for Fahrenheit
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        data = response.json()
+
+        if response.status_code == 200:
+            # Extract relevant information from the API response
+            temperature = data['main']['temp']
+            description = data['weather'][0]['description']
+
+            print(f"Weather in {city}: {description.capitalize()}, Temperature: {temperature}°C")
+        else:
+            print(f"Error: {data['message']}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def main():
+    # Replace 'your_api_key' with your actual OpenWeatherMap API key
+    api_key = '6ac51341fbe25af0043f9a7e166961c4'
+    city = input("Enter the city: ")
+
+    get_weather(api_key, city)
+
+if __name__ == "__main__":
+    main()
